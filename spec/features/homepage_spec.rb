f707d6e9b41686c8f7b743fadec56e35aa9f922c
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-12 19:58:24 -0500

    integration tests for homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -7,12 +7,12 @@ describe "Edit User Page" do
     sign_up
   end
 
-  it "should show slideshows when you are logged in" do
+  it "shouldnt give you the option to log in while logged in" do
     visit root_path
     page.should_not have_content "Log in"
   end
 
-   it "should show slideshows when you are logged in" do
+   it "should give you option to sign up if not logged in" do
     sign_out
     visit root_path
     page.should have_content "Not a user?"
