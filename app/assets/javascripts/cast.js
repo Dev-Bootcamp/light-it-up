526928ad9049895ceba71678e8e32ac473d4c15f
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-14 07:41:02 -0500

    casting slides auto-advances as expected

--- app/assets/javascripts/cast.js
+++ app/assets/javascripts/cast.js
@@ -1,23 +1,19 @@
 $(document).ready(function() {
-  
-  // for (i = 0; i < 100; i++) {
     
-    var $active = $('.active');
-    $('.hidden').hide();
- 
-    setTimeout( function() {
-      console.log('keyup');
-      $active = $('.active').next();
-      $active.prev().fadeOut();
-      $active.fadeIn();
-      $active.addClass('active');
-      $active.removeClass('hidden');
-      $active.prev().removeClass('active');
-      $active.prev().addClass('hidden');
-      $active.prev().appendTo('#slide_list');
-      }, 3000
-    );
+  var $active = $('.active');
+  $('.hidden').hide();
 
-  // };
+  setInterval( function() {
+    console.log('keyup');
+    $active = $('.active').next();
+    $active.prev().fadeOut();
+    $active.fadeIn();
+    $active.addClass('active');
+    $active.removeClass('hidden');
+    $active.prev().removeClass('active');
+    $active.prev().addClass('hidden');
+    $active.prev().appendTo('#slide_list');
+    }, 3000
+  );
  
 });
